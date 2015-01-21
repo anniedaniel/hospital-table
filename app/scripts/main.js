1a1b92b6e75d8a0d3bddc76e9e87dde7e65c6496
Annie Daniel <adaniel@texastribune.org>
2015-01-21 16:18:12 -0600

    search boxes and table is way prettier

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,28 @@
+/* global pym */
+
+var pymChild = new pym.Child();
+
 var $hospitalList = $('#hospital-list-all');
 
-$hospitalList.tablesorter({
-  sortList: [[1, 0]],
-  widgets: ['filter'],
+$(function(){
+  'use strict';
+  $hospitalList.tablesorter({
+    sortList: [[1, 0]],
+    widgets: ['filter'],
+    widgetOptions : {
+      // use the filter_external option OR use bindSearch function (below)
+      // to bind external filters.
+      // filter_external : '.search',
+
+      filter_columnFilters: false,
+      filter_saveFilters : true,
+      filter_reset: '.reset'
+    }
+  });
+
+  $.tablesorter.filter.bindSearch( $hospitalList, $('.table-search') );
+
+  $(window).load(function() {
+    pymChild.sendMessage('height', '' + document.body.scrollHeight);
+  });
 });
